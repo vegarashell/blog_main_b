a84b464af7274553914a58423945e735910656c7
vegarashell <alessandravega327@gmail.com>
2026-03-23 19:22:51 -0700

    hice cambios

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from.models import Category, Post, Comment
+
+class CommentItemInline(admin.Tabularinline):
+    model = Comment 
+    raw_id_fields = ['post']
+    
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title','intro', 'body']
+    list_display = ('title', 'category', 'created_at', 'status')
+    list_filter = ('category', 'created_at', 'status')
+    inlines = [CommentItemInline]
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ('title',)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'created_at')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# Create your views here.
+from django.db.models import Q
+from django.shortcuts import render,get_object_or_404,render
+
+from.models import Post, Category
 
 # Create your views here.
+def home(request):
+    posts=Post.objects.filter(status=Post.ACTIVE).order_by('created_at')
+    context={
+        'post':posts
+    }
+    return render(request,'blog/html',context)
